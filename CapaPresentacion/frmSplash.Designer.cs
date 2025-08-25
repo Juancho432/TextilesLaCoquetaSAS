898c0a97e9467d4e71d32a358431cb635431a22e
Emmanuel Mora <emmanuel.mora388@pascualbravo.edu.co>
2025-08-25 12:50:15 -0500

    Creacion formulario splash

---
+++ CapaPresentacion/frmSplash.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace CapaPresentacion
+{
+    partial class frmSplash
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pbSplash = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pbSplash)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbSplash
+            // 
+            this.pbSplash.Image = global::CapaPresentacion.Properties.Resources.Imagen_de_WhatsApp_2025_08_25_a_las_12_39_03_68168453;
+            this.pbSplash.Location = new System.Drawing.Point(-1, -1);
+            this.pbSplash.Name = "pbSplash";
+            this.pbSplash.Size = new System.Drawing.Size(800, 452);
+            this.pbSplash.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbSplash.TabIndex = 0;
+            this.pbSplash.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 2000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // frmSplash
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ControlBox = false;
+            this.Controls.Add(this.pbSplash);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmSplash";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            ((System.ComponentModel.ISupportInitialize)(this.pbSplash)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbSplash;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ CapaPresentacion/frmSplash.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CapaPresentacion
+{
+    public partial class frmSplash : Form
+    {
+        public frmSplash()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Stop(); //Detener el timer
+            frmPrincipal ppal = new frmPrincipal();
+            ppal.Show();
+            this.Hide();//Escondo formulario actual
+            
+        }
+    }
+}
