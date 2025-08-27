db17904e824dc6ba5e02c42621fa1f441ec67632
Emmanuel Mora <emmanuel.mora388@pascualbravo.edu.co>
2025-08-26 19:49:59 -0500

    Añadidas las clases Nomina y CD_Nomina en CapaDatos

---
+++ CapaDatos/CD_Nomina.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+
+namespace CapaDatos
+{
+    public class CD_Nomina
+    {
+        private Nomina nomina = new Nomina();
+        private string ruta = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "nomina.txt");
+
+        public void GuardarNomina(Nomina nomina)
+        {
+            string carpeta = Path.GetDirectoryName(ruta);
+            if (!Directory.Exists(carpeta))
+            {
+                Directory.CreateDirectory(carpeta);
+            }
+
+            File.AppendAllText(ruta, nomina.LineaArchivo() + Environment.NewLine);
+        }
+
+        public List<Nomina> ListarNomina()
+        {
+            var lista = new List<Nomina>();
+            if (!File.Exists(ruta)) return lista;
+
+            var lineas = File.ReadAllLines(ruta);
+            foreach (var linea in lineas)
+            {
+                lista.Add(nomina.SepararLinea(linea));
+            }
+            return lista;
+        }
+
+        public List<Nomina> BuscarNomina(string codigo)
+        {
+            var lista = new List<Nomina>();
+            if (!File.Exists(ruta)) return lista;
+
+            var lineas = File.ReadAllLines(ruta);
+            foreach (var linea in lineas)
+            {
+                var n = nomina.SepararLinea(linea);
+                if (n.Codigo == codigo)
+                {
+                    lista.Add(n);
+                }
+            }
+            return lista;
+        }
+
+    }
+}
---
+++ CapaDatos/Nomina.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    public class Nomina
+    {
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Cargo { get; set; }
+        public double SalarioBase { get; set; }
+        public double Salud { get; set; }
+        public double Pension { get; set; }
+        public double Auxilio { get; set; }
+        public double Neto { get; set; }
+
+        public Nomina() { }
+
+        public Nomina(Empleado empleado)
+        {
+            Codigo = empleado.Codigo;
+            Nombre = empleado.Nombre;
+            Apellido = empleado.Apellido;
+            Cargo = empleado.Cargo;
+            SalarioBase = double.Parse(empleado.Salario_base);
+
+            // Reglas de negocio
+            Salud = SalarioBase * 0.04;
+            Pension = SalarioBase * 0.04;
+            Auxilio = (SalarioBase < (2 * 1300000)) ? 200000 : 0; // suponiendo salario mínimo = 1,300,000
+            Neto = SalarioBase - Salud - Pension + Auxilio;
+        }
+
+        // Convierte en línea para archivo plano
+        public string LineaArchivo()
+        {
+            return $"{Codigo},{Nombre},{Apellido},{Cargo},{SalarioBase},{Salud},{Pension},{Auxilio},{Neto}";
+        }
+
+        // Leer desde línea del archivo
+        public Nomina SepararLinea(string linea)
+        {
+            var datos = linea.Split(',');
+            return new Nomina
+            {
+                Codigo = datos[0],
+                Nombre = datos[1],
+                Apellido = datos[2],
+                Cargo = datos[3],
+                SalarioBase = double.Parse(datos[4]),
+                Salud = double.Parse(datos[5]),
+                Pension = double.Parse(datos[6]),
+                Auxilio = double.Parse(datos[7]),
+                Neto = double.Parse(datos[8])
+            };
+        }
+
+    }//clase
+}
